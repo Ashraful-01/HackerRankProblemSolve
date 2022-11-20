1ecef5e47eb8f290f99000c1dd1f51244e447651
Ashraful Alam <61340373+Ashraful-01@users.noreply.github.com>
2022-11-20 23:49:55 +0600

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declare variable
+    int number;
+    long value;
+    char name;
+    float marks;
+    double hugenumber;
+
+    //taking input from users
+
+    cout << "Enter integer number: " ;
+    cin >> number;
+    cout << "Enter long: ";
+    cin >> value;
+    cout << "Enter char: ";
+    cin >> name;
+    cout << "Enter float: ";
+    cin >> marks;
+    cout << "Enter double: ";
+    cin >> hugenumber;
+
+
+return 0;
+}
